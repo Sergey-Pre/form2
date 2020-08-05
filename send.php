3d794190719a5c78c1b174b83d179606ad1ad8d4
Sergey-Pre <69147191+Sergey-Pre@users.noreply.github.com>
2020-08-05 12:15:25 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,35 +1,25 @@
-<?php
-/* Здесь проверяется существование переменных */
-if (isset($_POST['name'])) {$phone = $_POST['name'];}
-if (isset($_POST['phone'])) {$name = $_POST['phone'];}
- 
-/* Сюда впишите свою эл. почту */
-$myaddres  = "presnyakovsa286@gmail.com"; // кому отправляем
- 
-/* А здесь прописывается текст сообщения, \n - перенос строки */
-$mes = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name";
- 
-/* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ обратного звонка'; // от кого
-$send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
-ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
+ <?php
+// РЅРµСЃРєРѕР»СЊРєРѕ РїРѕР»СѓС‡Р°С‚РµР»РµР№
+$to  = 'presnyakovsa@gmail.com' . ', ';  // РѕР±СЂР°С‚РёС‚Рµ РІРЅРёРјР°РЅРёРµ РЅР° Р·Р°РїСЏС‚СѓСЋ
+$to .= 'presnyakovsa@gmail.com';
+
+// С‚РµРјР° РїРёСЃСЊРјР°
+$subject = 'РџРёСЃСЊРјРѕ СЃ РјРѕРµРіРѕ СЃР°Р№С‚Р°';
+
+// С‚РµРєСЃС‚ РїРёСЃСЊРјР°
+$message = 'РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ' . $_POST['name'] . ' РѕС‚РїСЂР°РІРёР» РІР°Рј РїРёСЃСЊРјРѕ:<br />' . $_POST['message'] . '<br />
+РЎРІСЏР·СЏС‚СЊСЃСЏ СЃ РЅРёРј РјРѕР¶РЅРѕ РїРѕ email <a href="mailto:' . $_POST['email'] . '">' . $_POST['email'] . '</a>'
+;
+
+// Р”Р»СЏ РѕС‚РїСЂР°РІРєРё HTML-РїРёСЃСЊРјР° РґРѕР»Р¶РµРЅ Р±С‹С‚СЊ СѓСЃС‚Р°РЅРѕРІР»РµРЅ Р·Р°РіРѕР»РѕРІРѕРє Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+// Р”РѕРїРѕР»РЅРёС‚РµР»СЊРЅС‹Рµ Р·Р°РіРѕР»РѕРІРєРё
+$headers .= 'To: РРІР°РЅ <Ivan@example.com>' . "\r\n"; // РЎРІРѕРµ РёРјСЏ Рё email
+$headers .= 'From: '  . $_POST['name'] . '<' . $_POST['email'] . '>' . "\r\n";
+
+
+// РћС‚РїСЂР°РІР»СЏРµРј
+mail($to, $subject, $message, $headers);
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="3; url=index.html">
-<title>Спасибо! Мы свяжемся с вами!</title>
-<meta name="generator">
-<script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
-/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
-</head>
-<body>
-<h1>Спасибо! Мы свяжемся с вами!</h1>
-</body>
-</html>
